58caa1f79fee97282d63d5701429ab78ac5944b6
davi.campanaro <davi.campanaro@041ET128060.catolica.ubec>
2025-09-03 09:27:37 -0300

    colocando a classe tarefasService

--- ToDoList/src/main/java/ucb/aplicacao/service/TarefasService.java
+++ ToDoList/src/main/java/ucb/aplicacao/service/TarefasService.java
@@ -2,13 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ucb.aplicacao.model;
+package ucb.aplicacao.service;
 
 /**
  *
  * @author davi.campanaro
  */
-public class Tarefas {
-
+public class TarefasService {
     
 }
