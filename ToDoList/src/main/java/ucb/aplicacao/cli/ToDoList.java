6d25796c9851aee968c113a36cfa5e66add460a4
davi.campanaro <davi.campanaro@041ET128060.catolica.ubec>
2025-09-03 09:13:12 -0300

    primeiro commit

---
+++ ToDoList/src/main/java/ucb/aplicacao/cli/ToDoList.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ucb.aplicacao.cli;
+
+/**
+ *
+ * @author davi.campanaro
+ */
+public class ToDoList {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
